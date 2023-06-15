3cec6f103e7695723941a6288ba95fe5db0f65bf
Eonvorax <mail4bishop@gmail.com>
2023-06-15 04:40:58 -0700

    Split fwrites and quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	#define B_SIZE 100
 	char quote;
 
-	quote[B_SIZE] = "and that piece of art is useful\" ";
+	quote[] = "and that piece of art is useful\" ";
 	fwrite(quote, sizeof(quote), 1, stderr);
-	quote[B_SIZE] = "- Dora Korpar, 2015-10-19\n";
+	quote[] = "- Dora Korpar, 2015-10-19\n";
 	fwrite(quote, sizeof(quote), 1, stderr);
-	return (0);
+	return (1);
 }
