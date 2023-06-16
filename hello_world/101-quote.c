ad8d6a359f58d219804b22ab66fdd9393c34642e
Eonvorax <mail4bishop@gmail.com>
2023-06-16 00:18:38 -0700

    Script 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,11 +6,9 @@
  */
 int main(void)
 {
-	char quote;
-
-	quote[50] = "and that piece of art is useful\" ";
+	char quote[50] = "and that piece of art is useful\" ";
 	fwrite(quote, sizeof(quote), 1, stderr);
-	quote[50] = "- Dora Korpar, 2015-10-19\n";
+	char quote[50] = "- Dora Korpar, 2015-10-19\n";
 	fwrite(quote, sizeof(quote), 1, stderr);
 	return (1);
 }
