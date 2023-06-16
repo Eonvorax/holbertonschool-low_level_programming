c2297184c1390a990f452baa4fff6b050f0ad6c0
Eonvorax <mail4bishop@gmail.com>
2023-06-16 00:35:05 -0700

    Fixed string length issue

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - prints a str without printf or puts
  *
@@ -6,8 +7,10 @@
  */
 int main(void)
 {
-	char quote_1[50] = "and that piece of art is useful\" ";
-	char quote_2[50] = "- Dora Korpar, 2015-10-19\n";
+	q1_len = strlen("and that piece of art is useful\" ");
+	q2_len = strlen("- Dora Korpar, 2015-10-19\n");
+	char quote_1[q1_len] = "and that piece of art is useful\" ";
+	char quote_2[q2_len] = "- Dora Korpar, 2015-10-19\n";
 
 	fwrite(quote_1, sizeof(quote_1), 1, stderr);
 	fwrite(quote_2, sizeof(quote_2), 1, stderr);
