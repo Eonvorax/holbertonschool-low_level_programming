6b8978bd9fdad4877366492e93f709beab66f859
Eonvorax <mail4bishop@gmail.com>
2023-06-19 02:48:30 -0700

    Script 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 from 0 to 9 with
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
