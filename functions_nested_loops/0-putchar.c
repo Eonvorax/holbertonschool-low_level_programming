54ca29c2996015c3251bff3b209d6e78a8a1240f
Eonvorax <mail4bishop@gmail.com>
2023-06-19 06:21:33 -0700

    Script 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-
+#include <string.h>
 /**
- * main - prints _putchar and a newline
+ * main - prints "_putchar" and a newline
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[12] = "_putchar";
+	int i, n;
+
+	n = strlen(str);
+	for (i = 0; i < n; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
