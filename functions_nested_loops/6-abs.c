19ea1e11c49b2040994ff4e22aae07049c0b4cde
Eonvorax <mail4bishop@gmail.com>
2023-06-20 00:28:34 -0700

    Script 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value of n
+ * @n : integer n
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
