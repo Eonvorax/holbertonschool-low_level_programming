6e1691fe64d7a71c812fb8ff5ff53b811a773c73
Eonvorax <mail4bishop@gmail.com>
2023-06-20 12:26:55 -0700

    Script 11 : fixed -10x

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * print_to_98 - prints the count from n to 98
  * @n : integer n
@@ -9,9 +10,7 @@ void print_to_98(int n)
 	int res, div, inc, cnt;
 
 	if (n > 98)
-	{
 		inc = -1;
-	}
 	else if (n < 98)
 	{
 		inc = 1;
@@ -21,19 +20,25 @@ void print_to_98(int n)
 	{
 		res = cnt;
 		div = 1;
-		while ((div * 10) < res)
+		if (cnt < 0)
 		{
-			div *= 10;
+			_putchar('-');
+			res = abs(res);
 		}
 		if (res == 0)
-		{
 			_putchar('0');
-		}
-		while (res != 0)
+		else
 		{
-			_putchar((res / div) + '0');
-			res %= div;
-			div /= 10;
+			while ((div * 10) <= res)
+			{
+				div *= 10;
+			}
+			while (div > 0)
+			{
+				_putchar((abs(res / div)) + '0');
+				res %= div;
+				div /= 10;
+			}
 		}
 		_putchar(',');
 		_putchar(' ');
