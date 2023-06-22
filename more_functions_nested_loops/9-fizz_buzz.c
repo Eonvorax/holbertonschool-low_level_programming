8bbffc2df35dd458a3be77a8adc89e9564bf7429
Eonvorax <mail4bishop@gmail.com>
2023-06-22 05:08:39 -0700

    Script 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * fizz_buzz - prints numbers from 1 to 100 and FizzBuzz
+ * Return: nothing
+ */
+void fizz_buzz(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (((n % 3) == 0) && ((n % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code
+ * Return: always 0
+ */
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
