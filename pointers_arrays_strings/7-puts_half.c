217fe562809cd8c0db04e08db32d86049f6a19d1
Eonvorax <mail4bishop@gmail.com>
2023-06-26 07:58:39 -0700

    TODO : Script 7 (segfault)

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - prints a string in reverse
+ * @s : pointer to string
+ * Return: nothing
+ */
+void puts_half(char *s)
+{
+	int beg, end;
+
+	end = 0;
+	while ((s[end] != '\0'))
+	{
+		end++;
+	}
+	while (beg < ((end - 1) / 2))
+	{
+		beg++;
+	}
+	while ((s[beg] != '\0'))
+	{
+		_putchar(s[beg]);
+		beg++;
+	}
+	_putchar('\n');
+}
