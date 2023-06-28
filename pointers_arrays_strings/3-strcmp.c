cc56671aaca0ccf81d6ab9681dffd83a88caf783
Eonvorax <mail4bishop@gmail.com>
2023-06-28 01:42:13 -0700

    Adjusted for the fact that the task is wrong

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,12 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int cnt = 0;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
-		if (s1[i] == s2[i])
-			cnt++;
 	}
-	if (s1[i] == '\0' && s2[i] == '\0' && cnt == i)
-		return (0);
-	else if (s1[i] == '\0' && s2[i] != '\0' && cnt != i)
-		return (-1);
-	else if (s1[i] != '\0' && s2[i] == '\0' && cnt != i)
-		return (1);
 	return (0);
 }
