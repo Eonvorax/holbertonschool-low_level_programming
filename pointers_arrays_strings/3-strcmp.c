cd673c16002cbef6c8b072c645f1a8f3bb7ec950
Eonvorax <mail4bishop@gmail.com>
2023-06-28 01:17:35 -0700

    Changed default condition

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -20,7 +20,6 @@ int _strcmp(char *s1, char *s2)
 		return (0);
 	else if (s1[i] == '\0' && s2[i] != '\0')
 		return (-1);
-	else if (s2[i] == '\0' && s1[i] != '\0')
+	else
 		return (1);
-	return (0);
 }
