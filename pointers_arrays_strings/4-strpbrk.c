9334dff0416e1aadb5112c7e87df6255f5419870
Eonvorax <mail4bishop@gmail.com>
2023-06-29 11:11:42 -0700

    Script 4 ter

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - search string for first occurence of any char of accept set
+ * @s : pointer to string searched
+ * @accept : pointer to set of chars to search
+ * Return: pointer to first match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+			i++;
+		}
+		s++;
+	}
+	return (s);
+}
