d1d327af04e61667f0657f3cf620e9a94e21f951
Eonvorax <mail4bishop@gmail.com>
2023-07-03 07:45:59 -0700

    TODO finish algorithm

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the square root
+ * @n : given int
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion((n - 1) * (n - 1)));
+}
