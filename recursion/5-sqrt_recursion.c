4ef2a13946ec1dd6cd3797110db93b420f5d0160
Eonvorax <mail4bishop@gmail.com>
2023-07-03 12:08:44 -0700

    Script 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,14 +1,28 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the square root
+ * helper - looks for sqrt with the input number from n to comp
+ * @i:  int number squared and compared against comp
+ * @comp: int number to compare with
+ * Return: natural sqrt of number comp
+ */
+int helper(int i, int comp)
+{
+	if (i * i == comp)
+	{
+		return (i);
+	}
+	if (i * i > comp)
+	{
+		return (-1);
+	}
+	return (helper(i + 1, comp));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of n
  * @n : given int
  * Return: int
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	return (_sqrt_recursion((n - 1) * (n - 1)));
+	return (helper(1, n));
 }
