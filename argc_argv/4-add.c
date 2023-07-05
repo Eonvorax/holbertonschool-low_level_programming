0b67662f4b69725165fb66e2c995cdb72d226b1e
Eonvorax <mail4bishop@gmail.com>
2023-07-05 07:18:07 -0700

    Handled complex error case

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,15 +7,21 @@
  */
 int add(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		char * t_arg;
+
+		t_arg = argv[i];
+		for (j = 0; (t_arg[j]) != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if ((t_arg[j]) < '0' || (t_arg[j]) > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
