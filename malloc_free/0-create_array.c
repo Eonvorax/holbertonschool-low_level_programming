a7c0217b4c27b07c51820cd64465535184ddd6d5
Eonvorax <mail4bishop@gmail.com>
2023-07-10 01:25:58 -0700

    Script 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars, initialized with a specific char
+ * @size : unsigned int
+ * @c : char
+ * Return: char, NULL if size = 0 or in case of failure
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
