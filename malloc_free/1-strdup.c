bb197d2b5b2376375fd895b0fde4619c0e645a92
Eonvorax <mail4bishop@gmail.com>
2023-07-10 02:21:43 -0700

    Script 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strdup - returns pointer to copy of given string
+ * @str : pointer to given string
+ * Return: pointer to duplicate string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int size;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (size = 0; str[size] != '\0'; size++)
+		;
+	dup = malloc(sizeof(char) * size);
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		dup[i] = str[i];
+	}
+	return (dup);
+}
