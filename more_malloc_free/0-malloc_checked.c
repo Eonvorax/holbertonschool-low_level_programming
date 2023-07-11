f06cb3f9a86e9e415310a9cad2c56022974dd944
Eonvorax <mail4bishop@gmail.com>
2023-07-11 05:04:52 -0700

    Script 1 and _putchar

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,17 @@
 #include "main.h"
+/**
+ * malloc_checked - allocates memory and exits with status
+ * value of 98 in case of failure
+ * @b : unsigned int size (in bytes) of memory to allocate
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+	return (mem);
+}
