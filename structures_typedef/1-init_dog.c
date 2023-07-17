bc552c58bd75df288001aac7e6d5d0ea6f7755c0
Eonvorax <mail4bishop@gmail.com>
2023-07-17 01:31:03 -0700

    Script 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to dog structure
+ * @name: string name of dog
+ * @age: float age of dog
+ * @owner: string owner of dog
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
