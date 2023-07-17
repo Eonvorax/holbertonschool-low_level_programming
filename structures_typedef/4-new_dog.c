e188f35f3f5587088a3bebf53946d9cf9f7dac5b
Eonvorax <mail4bishop@gmail.com>
2023-07-17 05:16:21 -0700

    Script 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,28 +9,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	unsigned int name_size, owner_size;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
 		return (NULL);
 	}
-	if (name != NULL)
+	for (name_size = 0; name[name_size] != '\0'; name_size++)
+		;
+	d->name = malloc(sizeof(char) * (name_size + 1));
+	if (d->name == NULL)
 	{
-		d->name = name;
+		free(d->name);
+		free(d);
+		return (NULL);
 	}
-	else
+	for (owner_size = 0; owner[owner_size] != '\0'; owner_size++)
+		;
+	d->owner = malloc(sizeof(char) * (owner_size + 1));
+	if (d->owner == NULL)
 	{
-		d->name = NULL;
+		free(d->owner);
+		free(d);
+		return (NULL);
 	}
+	for (name_size = 0; name[name_size] != '\0'; name_size++)
+		d->name[name_size] = name[name_size];
 	d->age = age;
-	if (owner != NULL)
-	{
-		d->owner = owner;
-	}
-	else
-	{
-		d->owner = NULL;
-	}
+	for (owner_size = 0; owner[owner_size] != '\0'; owner_size++)
+		d->owner[owner_size] = owner[owner_size];	
 	return (d);
 }
