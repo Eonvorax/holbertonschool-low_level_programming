d5dae5e109d4131d5ce4807d36c59cfa8f830f37
Eonvorax <mail4bishop@gmail.com>
2023-07-17 05:46:47 -0700

    Script 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,35 +9,32 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
-	unsigned int name_size, owner_size;
+	int name_size, owner_size;
 
+	name_size = 0;
+	owner_size = 0;
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
 		return (NULL);
 	}
-	for (name_size = 0; name[name_size] != '\0'; name_size++)
+	for (; name[name_size] != '\0'; name_size++)
+		;
+	for (; owner[owner_size] != '\0'; owner_size++)
 		;
 	d->name = malloc(sizeof(char) * (name_size + 1));
-	if (d->name == NULL)
+	d->owner = malloc(sizeof(char) * (owner_size + 1));
+	if (d->name == NULL || d->owner == NULL)
 	{
 		free(d->name);
-		free(d);
-		return (NULL);
-	}
-	for (owner_size = 0; owner[owner_size] != '\0'; owner_size++)
-		;
-	d->owner = malloc(sizeof(char) * (owner_size + 1));
-	if (d->owner == NULL)
-	{
 		free(d->owner);
 		free(d);
 		return (NULL);
 	}
 	for (name_size = 0; name[name_size] != '\0'; name_size++)
 		d->name[name_size] = name[name_size];
-	d->age = age;
 	for (owner_size = 0; owner[owner_size] != '\0'; owner_size++)
-		d->owner[owner_size] = owner[owner_size];	
+		d->owner[owner_size] = owner[owner_size];
+	d->age = age;
 	return (d);
 }
