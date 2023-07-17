3ca04c98929e2f9bcb9b771e1efb7215ce53152c
Eonvorax <mail4bishop@gmail.com>
2023-07-17 06:31:02 -0700

    Script 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * free_dog - frees a dog_t structure
+ * @d: pointer to dog_t structure
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
