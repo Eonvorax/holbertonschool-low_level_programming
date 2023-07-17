57a7a10b9c62ffd3330d46bc5a688c0871374a7c
Eonvorax <mail4bishop@gmail.com>
2023-07-17 01:00:39 -0700

    Dir + Script 0 + README

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - stores information about a VERY good boy
+ * @name: string name of the dog
+ * @age: float age of the dog
+ * @owner: string name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
