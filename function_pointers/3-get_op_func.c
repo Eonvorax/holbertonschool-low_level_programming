94e5506cc74550e21e7afcf01f1cd13bb1448275
Eonvorax <mail4bishop@gmail.com>
2023-07-18 04:50:17 -0700

    Task 3: 1.0

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * get_op_func - selects and runs the correct function for the operation
+ * @s: pointer to string (operator)
+ * Return: pointer to correct int (int, int) function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (s[0] == *(ops[i].op))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (ops[i].f);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+/**
+ * main - performs simple operations then print the result
+ * @argc: int number of arguments, including file name
+ * @argv: pointer to array of string arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	op_t calc;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	calc.op = argv[2];
+	if (calc.op == NULL || get_op_func(calc.op) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((calc.op[0] == '/' || calc.op[0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	calc.f = get_op_func(calc.op);
+	printf("%d\n", (*(calc.f))(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
