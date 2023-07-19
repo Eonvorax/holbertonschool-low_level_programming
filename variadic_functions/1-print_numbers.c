52915b3be2a1c12fa886b78f914d11d06e527056
Eonvorax <mail4bishop@gmail.com>
2023-07-19 02:06:39 -0700

    Fix: correct NULL separator output

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,18 +11,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator != NULL)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
+		printf("%d", va_arg(args, int));
+		if (i != (n - 1) && separator != NULL)
 		{
-			printf("%d", va_arg(args, int));
-			if (i != (n - 1))
-			{
-				printf("%s", separator);
-			}
+			printf("%s", separator);
 		}
-		printf("\n");
-		va_end(args);
 	}
+	printf("\n");
+	va_end(args);
 }
