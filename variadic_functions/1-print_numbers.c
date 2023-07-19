83571d83dcb735f07ed9285b0658a3bc184036f2
Eonvorax <mail4bishop@gmail.com>
2023-07-19 02:03:37 -0700

    Task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers with string separators, then newline
+ * @separator: pointer to string separator
+ * @n: number of integers passed to function
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (separator != NULL)
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+			if (i != (n - 1))
+			{
+				printf("%s", separator);
+			}
+		}
+		printf("\n");
+		va_end(args);
+	}
+}
