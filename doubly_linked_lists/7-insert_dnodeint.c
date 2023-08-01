3f12f6211eb55a28c22c47b2e64e446e888d95e6
Eonvorax <mail4bishop@gmail.com>
2023-08-01 12:40:02 -0700

    Fix: tmp != NULL to ensure no wrong NULL return

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 	tmp = *h;
-	while (count < idx)
+	while (count < idx && tmp != NULL)
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
