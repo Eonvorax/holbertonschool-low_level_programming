8706d3ebb27faa66c1e181e5ae16f7118c5c2a4e
Eonvorax <mail4bishop@gmail.com>
2023-08-01 12:00:18 -0700

    Remade with tmp NULL handling in loop

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,36 +13,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *tmp;
 	unsigned int count = 0;
 
-	if (h == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(dlistint_t) * 1);
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 	if (*h == NULL)
 	{
 		*h = new_node;
+		return (new_node);
 	}
 	tmp = *h;
-	while (tmp != NULL && count != idx)
+	while (count < idx)
 	{
 		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 		count++;
 	}
-	if (count == idx)
-	{
-		new_node->prev = tmp->prev;
-		new_node->next = tmp;
-		tmp->prev = new_node;
-		if (new_node->prev != NULL)
-			(new_node->prev)->next = new_node;
-		else
-			*h = new_node;
-	}
+
+	new_node->prev = tmp->prev;
+	new_node->next = tmp;
+	tmp->prev = new_node;
+	if (new_node->prev != NULL)
+		new_node->prev->next = new_node;
 	else
-	{
-		free(new_node);
-		return (NULL);
-	}
+		*h = new_node;
 	return (new_node);
 }
