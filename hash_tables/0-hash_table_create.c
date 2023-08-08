ccc0caeb87e050ece91120ec6bf94c8355f20dfd
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-08-08 19:39:11 +0000

    Task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,11 +8,21 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t ht;
+	hash_table_t *ht = NULL;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
-    hash_table_t = malloc(sizeof(size) + sizeof(hash_node_t) * size)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
 }
